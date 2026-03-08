df9a47fd4b8e44ea12e8b1f6ffe8a1dd49f09271
nadfil96 <nadjafilipsen@hotmail.com>
2026-03-08 11:36:16 +0100

    Update mission_06.sql

--- mission_06.sql
+++ mission_06.sql
@@ -5,6 +5,7 @@ walking_required,
 binoculars
 FROM mission_hint;
 
+--Result:
 location | wildlife | walking_required | binoculars
 ---------|----------|------------------|-----------
 coast    | possible | true             | useful
