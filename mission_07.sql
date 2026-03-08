2986c8b2a01771cff65865fbf46def36648a30f9
nadfil96 <nadjafilipsen@hotmail.com>
2026-03-08 11:08:22 +0100

    Create mission_07.sql

---
+++ mission_07.sql
@@ -0,0 +1,32 @@
+SELECT
+arrival_time,
+travel_time,
+arrival_time + travel_time AS departure_time
+FROM trip;
+
+arrival_time | travel_time | departure_time
+-------------|-------------|---------------
+11           | 1           | 12
+
+-- ERROR: query incorrect
+
+
+Vi ska vara framme kl 11.
+Resan tar 1 timme.
+
+Vilken tid ska vi åka?
+
+
+
+
+MISSION STATUS: UNLOCKED
+
+PACKLIST VERIFIED:
+
+✓ hiking_clothes
+✓ swimwear
+✓ dinner_clothes
+✓ comfy_clothes
+
+EXECUTE birthday_adventure;
+
