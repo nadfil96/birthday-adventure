4b49292a3b6d4a43673c1560d5c93938e0e89538
nadfil96 <nadjafilipsen@hotmail.com>
2026-03-08 11:03:53 +0100

    Create mission_04.sql

---
+++ mission_04.sql
@@ -0,0 +1,23 @@
+SELECT
+event,
+time,
+dresscode
+FROM evening_plan;
+
+event       | time    | dresscode
+------------|---------|----------
+restaurant  | evening | not_casual
+
+
+SELECT clothing
+FROM gear
+WHERE event='restaurant';
+
+SELECT location
+FROM house
+WHERE comfort='maximum';
+
+sofa
+
+
+
