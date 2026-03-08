cee7a8cc685197e9595f419c21bc37ce8dec5aba
nadfil96 <nadjafilipsen@hotmail.com>
2026-03-08 11:34:04 +0100

    Update mission_03.sql

--- mission_03.sql
+++ mission_03.sql
@@ -4,9 +4,11 @@ temperature,
 relaxation
 FROM environment;
 
+--Result:
 water | temperature | relaxation
 ------|-------------|-----------
 true  | warm        | high
+
 
 SELECT clothing
 FROM gear
@@ -14,8 +16,7 @@ WHERE water = true;
 
 
 
-
-
+--Next:
 SELECT location
 FROM house
 WHERE contains='clothes';
